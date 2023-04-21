e3f8a31e2f7b3800cff605f341a735dd863ac45c
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-04-21 10:35:52 +0100

    corriger templater.php

--- view/template.php
+++ view/template.php
@@ -4,33 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= $titre ?></title>
 </head>
 <body>
-<?php
-ob_start();?>
-<p class="uk-label uk-label-warning">Il y a <?=$requete->rowCount()?>films</p>
-<table class="uk-table uk-table-striped">
-    <thead>
-        <tr>
-            <th>TITRE</th>
-            <th>ANNE SORTIE</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php 
-        foreach($requete->fetchAll() as $film){?>
-        <tr>
-            <td><?= $film["titre"]?></td>
-            <td><?= $film["anne_sortie_fr"]?></td>
-        </tr>
-        <?php } ?>
-        
-    </tbody>
-</table>
-<?php
-$titre="Liste des films";
-$titre_secondaire="Liste des films";$contenu=ob_get_clean();
-require "view/template.php";?>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php?action=listFilms">Liste des films</a>
+            </li>
+            <li>
+                <a href="index.php?action=listActeurs">Liste des acteurs</a>
+            </li>
+            <li>
+                <a href=""></a>
+            </li>
+            <li>
+                <a href=""></a>
+            </li>
+        </ul>
+    </nav>
+    <h1><?= $titre_secondaire ?></h1>
+    
+    <main>
+        <?= $contenu ?>
+    </main>
 </body>
 </html>
