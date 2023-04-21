432490f9eac906a807d2c906988b166e6f8497fa
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-04-21 10:34:48 +0100

    méthode liste acteurs

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -10,5 +10,14 @@ class CinemaController{
         $requete=$pdo->query("
         SELECT titre, annee_sortie_fr FROM film");
         require "view/listFilms.php";
+    }   
+
+    public function listActeurs() {
+        $pdo=Connect::seConnecter();
+        $requete=$pdo->query("
+        SELECT * 
+        FROM  personne p
+        INNER JOIN acteur a ON a.id_personne = p.id_personne");
+        require "view/listActeurs.php";
     }
 }
